8e9b28dfc7044eb62d5d9ff11e61fe91f83a7ecc
suibingbing <suibingbing@piesat.cn>
2024-06-26 14:55:04 +0800

    删除旧版本cmake工程

--- build/util/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/geos_VS2022/util
-# Build directory: E:/geos_VS2022/build/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("geosop")
